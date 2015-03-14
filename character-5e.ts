bf1011a9a2f4cdf573069bd749bb44f8ee23aa90
Jason Meisel <jason.meisel@gmail.com>
2015-03-13 19:21:20 -0700

    some more fields

--- character-5e.ts
+++ character-5e.ts
@@ -1,9 +1,33 @@
 /// <reference path="typings/tsd.d.ts" />
 
+interface ClassLevel
+{
+  name : string;
+  level : number;
+}
+
+interface Abilities
+{
+  strength : number;
+  dexterity : number;
+  constitution : number;
+  intelligence : number;
+  wisdom : number;
+  charisma : number;
+}
+
 interface Character
 {
   _id ?: string;
   name : string;
+  classes : ClassLevel[];
+  background : string;
+  playerName : string;
+  race : string;
+  alignment : string;
+  xp : number;
+  abilities : Abilities;
+  // TODO: proficiencies?
 }
 
 declare var CharacterDB : Mongo.Collection<Character>;
